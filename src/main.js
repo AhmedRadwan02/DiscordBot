170f0b4e0389d04d6d12fcf7a6d575a377189196
AhmedRadwan02 <ahmedyradwan02@gmail.com>
2022-05-12 23:11:09 +0300

    Co-authored-by: Khaled <KhaledMAlasmari@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
+// Imports
 import {
 	Client,
 	Intents
 } from "discord.js"
+
 import dotenv from 'dotenv'
+
+import {
+	joinVoiceChannel,
+	VoiceConnectionStatus,
+	entersState,
+	getVoiceConnection
+} from '@discordjs/voice'
+
+// dotenv connection
 dotenv.config()
 
 // Create a new client instance
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS]
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
 });
 
+// token
 const token = process.env.token
+
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -23,7 +36,7 @@ client.on('interactionCreate', async interaction => {
 	const {
 		commandName
 	} = interaction;
-
+	// Commands
 	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
 	} else if (commandName === 'server') {
@@ -31,11 +44,57 @@ client.on('interactionCreate', async interaction => {
 	} else if (commandName === 'user') {
 		await interaction.reply('User info.');
 	} else if (commandName === 'play') {
-		 //If you are not in the voice channel, then return a message
-		//if (!message.member.voice.channel) return message.channel.send("Please connect to a voice channel :c !");
-		//message.member.voice.channel.join();
+		playAudio(interaction);
+	} else if (commandName === 'stop'){
+		stop(interaction);
 	}
+
 });
 
+function stop(interaction){
+	const voiceChannel = interaction.member.voice.channel;
+
+	if (voiceChannel) {
+		const connection = getVoiceConnection(voiceChannel.guild.id)
+		connection.destroy()
+	}
+
+}
+const playAudio = (interaction) => {
+	// declare vc
+	const voiceChannel = interaction.member.voice.channel;
+	if (voiceChannel) {
+		const permissions = voiceChannel.permissionsFor(interaction.client.user);
+		if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+			return interaction.channel.send(
+				"I need the permissions to join and speak in your voice channel!"
+			);
+		}
+		const connection = joinVoiceChannel({
+			channelId: voiceChannel.id,
+			guildId: voiceChannel.guild.id,
+			adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+		});
+
+		// handle disconnecting?
+		connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+			try {
+				await Promise.race([
+					entersState(connection, VoiceConnectionStatus.Signalling, 5000),
+					entersState(connection, VoiceConnectionStatus.Connecting, 5000),
+				]);
+				// Seems to be reconnecting to a new channel - ignore disconnect
+			} catch (error) {
+				// Seems to be a real disconnect which SHOULDN'T be recovered from
+			}
+		});
+
+
+
+	}
+	else {
+		return interaction.channel.send("You need to be in a voice channel to play music!");
+	}
+}
 // Login to Discord with your client's token
 client.login(token);
